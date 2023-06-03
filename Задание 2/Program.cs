6471d94e93f6e6f9c36ca08ed5c6ea5d0dc4cf37
Sergey Z <drakon.s120276@gmail.com>
2023-06-03 15:45:39 +0300

    Добавьте файлы проекта.

---
+++ Задание 2/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.OutputEncoding = System.Text.Encoding.UTF8;
+{
+
+    double a = 3.141592653;
+    double s = 2.7182818284590452;
+
+    Console.WriteLine(a);
+    Console.WriteLine(s);
+
+
+}
